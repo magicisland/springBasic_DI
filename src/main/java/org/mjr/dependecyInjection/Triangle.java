5ff81ccbb0a0849b7f07bfb1417ce8a7cc15ad10
magicisland <marcelojavierrodriguez@gmail.com>
2017-10-10 11:00:08 -0300

    injection dependencies - Constructor Injection

--- src/main/java/org/mjr/dependecyInjection/Triangle.java
+++ src/main/java/org/mjr/dependecyInjection/Triangle.java
@@ -3,21 +3,44 @@ package org.mjr.dependecyInjection;
 public class Triangle {
 
 	protected String type;
+	protected int height;
 	
 	
+	public Triangle(String type, int height) {
+		super();
+		this.type = type;
+		this.height = height;
+	}
+
+
+
+	public int getHeight() {
+		return height;
+	}
+
+
+
 	public String getType() {
 		return type;
 	}
 
+	
 
-	public void setType(String type) {
+	public Triangle(String type) {
+		super();
 		this.type = type;
 	}
 
 
+
+	/*public void setType(String type) {
+		this.type = type;
+	}
+*/
+
 	public void draw(){
 		
-		System.out.println(" Triangle type : "  + getType());
+		System.out.println(" Triangle type : "  + getType() + "  " +  getHeight());
 		
 	}
 }
