278f7c36b4a7cac4ef3ab51c0953652806793aa2
magicisland <marcelojavierrodriguez@gmail.com>
2017-10-10 11:58:27 -0300

    just a point

---
+++ src/main/java/org/mjr/dependecyInjection/Point.java
@@ -0,0 +1,25 @@
+package org.mjr.dependecyInjection;
+
+public class Point {
+
+	protected int x;
+	protected int y;
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	@Override
+	public String toString() {
+		return "Point [x=" + x + ", y=" + y + "]";
+	}
+	
+	
+}
